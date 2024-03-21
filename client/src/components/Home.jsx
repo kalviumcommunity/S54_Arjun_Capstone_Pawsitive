7c7505ca99e9fd514b8c51ce61f73b897c447080
Arjun Kotha <141010334+Phoenix1531@users.noreply.github.com>
2024-03-21 17:10:46 +0530

    firebase authentication implemented

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -5,16 +5,16 @@ import DC from '../assets/DC.svg'
 
 const Home = () => {
   return (
-    <div>
+    <>
       <Navbar />
       <Box display='flex' flexDirection={{ base: 'column', md: 'row' }} p={{ base: '3vw', md: '2rem' }}>
         <Image src={DC} h={{ base: '35vh', md: '35vw' }} m={{ base: '1rem', md: '2rem' }} alignSelf="center" />
-        <Box display='flex' flexDirection='column' alignItems="center" justifyContent="center" textAlign='center' p={{ base: '5vw', md: '3vw' }}>
+        <Box display='flex' flexDirection='column' alignItems="center" justifyContent="center" textAlign='left' p={{ base: '5vw', md: '3vw' }}>
           <Heading size={{ base: '2xl', md: '3xl' }} fontWeight='bold' mb={{ base: '1rem', md: '2rem' }}>Welcome to Pawsitive where we spread <Heading size={{ base: '2xl', md: '3xl' }} className='paytone-one-regular' id='yellow'>Pawsitivity :)</Heading> </Heading>
           <Text fontSize={{ base: '3.9vw', md: '2xl' }}>Join our compassionate community dedicated to rescuing, adopting, and loving for animals in need. Together, we're shaping a brighter, kinder world for our furry friends.</Text>
         </Box>
       </Box>
-    </div>
+    </>
   )
 }
 
