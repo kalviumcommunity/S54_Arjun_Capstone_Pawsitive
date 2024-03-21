c8ea9dcfea4e65b9ce94afa95e57ce85e540a84a
Arjun Kotha <141010334+Phoenix1531@users.noreply.github.com>
2024-03-21 17:10:46 +0530

    firebase authentication implemented

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,47 +1,62 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { signOut } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 import {
     Box,
     Flex,
     HStack,
     Button,
+    Stack,
+    Icon,
     Text,
     Link,
     Menu,
     MenuButton,
     MenuList,
     MenuItem,
-    Stack,
-    Icon,
-    IconButton,
     useDisclosure,
     useColorModeValue,
-    Image
+    IconButton,
+    Drawer,
+    DrawerOverlay,
+    DrawerContent,
+    DrawerHeader,
+    DrawerBody,
+    DrawerCloseButton,
 } from '@chakra-ui/react';
 import { GiHamburgerMenu } from 'react-icons/gi';
 import { AiOutlineClose } from 'react-icons/ai';
 import { BiChevronDown } from 'react-icons/bi';
-import { RiFlashlightFill } from 'react-icons/ri';
-import logo from '../assets/logo.png'
+import logo from '../assets/logo.png';
+import { AppContext } from '../context/ParentContext';
+import { auth } from '../firebase/config';
+
 const navLinks = [
     { name: 'Donate', path: '/Donate' },
     { name: 'Adopt', path: '/Adopt' },
     { name: 'Blog', path: '/Blog' },
 ];
 
-const dropdownLinks = [
-    { name: 'Explore', path: '/Community' },
-    { name: 'Message', path: '/Community/Message' },
-    { name: 'Create', path: '/Community/Create' }
-];
-
 export default function Navbar() {
     const { isOpen, onOpen, onClose } = useDisclosure();
+    const Navigate = useNavigate();
+    const { signin, setSignin } = useContext(AppContext);
+
+    const handleSignOut = async () => {
+        try {
+            setSignin(false)
+            await signOut(auth)
+            console.log("logout successful")
+            Navigate('/signup')
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
     return (
         <Box p={4} bg={useColorModeValue('white', 'gray.800')} w={"100vw"}>
             <Flex h={16} alignItems="center" justifyContent="space-between" mx="auto" >
-                <Link to={'/'} ml={"1vw"} textDecoration={"none"} _hover={"none"}>
-                    {/* <Image src={logo} h={20} w={220} /> */}
+                <Link href={'/'} ml={"1vw"} textDecoration={"none"} _hover={"none"}>
                     <h1 className='paytone-one-regular'><span id='yellow'>PAW</span>SITIVE</h1>
                 </Link>
                 <HStack spacing={8} alignItems="center" mr={35} >
@@ -50,71 +65,58 @@ export default function Navbar() {
                             <NavLink key={index} {...link} onClose={onClose} />
                         ))}
 
-                        {/* Dropdown Menu */}
-                        <Menu autoSelect={false} isLazy>
-                            {({ isOpen, onClose }) => (
-                                <>
-                                    <MenuButton _hover={{ color: 'black',fontSize:"lg" }}>
-                                        <Flex alignItems="center">
-                                            <Text>Community</Text>
-                                            <Icon
-                                                as={BiChevronDown}
-                                                h={5}
-                                                w={5}
-                                                ml={1}
-                                                transition="all .25s ease-in-out"
-                                                transform={isOpen ? 'rotate(180deg)' : ''}
-                                            />
-                                        </Flex>
-                                    </MenuButton>
-                                    <MenuList
-                                        zIndex={5}
-                                        bg={useColorModeValue('rgb(255, 255, 255)', 'rgb(26, 32, 44)')}
-                                        border="none"
-                                        boxShadow={useColorModeValue(
-                                            '2px 4px 6px 2px rgba(160, 174, 192, 0.6)',
-                                            '2px 4px 6px 2px rgba(9, 17, 28, 0.6)'
-                                        )}
-                                    >
-                                        {dropdownLinks.map((link, index) => (
-                                            <MenuLink key={index} name={link.name} path={link.path} onClose={onClose} />
-                                        ))}
-                                    </MenuList>
-                                </>
-                            )}
+
+                        <Menu>
+                            <MenuButton outline={'none'} >
+                                <HStack><Text _hover={{ fontSize: "lg" }}>Community</Text><BiChevronDown /></HStack>
+                            </MenuButton>
+                            <MenuList>
+                                <Link href='/community/explore'><MenuItem>Explore</MenuItem></Link>
+                                <Link href='/community/message'><MenuItem>Message</MenuItem></Link>
+                                <Link href='/community/create'><MenuItem>Create</MenuItem></Link>
+                            </MenuList>
                         </Menu>
                     </HStack>
                 </HStack>
 
-                <Button bg={"#FBBC05"} size="md" borderRadius={"20px"} display={{ base: 'none', md: 'block' }} _hover={"none"} mr={30}>
-                    Sign in
-                </Button>
+                {signin ? <Button onClick={handleSignOut} bg={"#FBBC05"} size="lg" borderRadius={"30px"} display={{ base: 'none', md: 'block' }} _hover={"none"} mr={30} color={'white'} outline={'none'}>
+                    Log out
+                </Button> : <Button onClick={() => { Navigate('/signup') }} bg={"#FBBC05"} size="lg" borderRadius={"30px"} display={{ base: 'none', md: 'block' }} _hover={"none"} mr={30} color={'white'} outline={'none'}>
+                    Sign up
+                </Button>}
                 <IconButton
                     size="md"
-                    icon={isOpen ? <AiOutlineClose /> : <GiHamburgerMenu />}
+                    icon={<GiHamburgerMenu />}
                     aria-label="Open Menu"
                     display={{ base: 'inherit', md: 'none' }}
-                    onClick={isOpen ? onClose : onOpen}
+                    onClick={onOpen}
                 />
             </Flex>
 
-            {/* Mobile Screen Links */}
             {isOpen ? (
-                <Box pb={4} display={{ base: 'inherit', md: 'none' }} textAlign={"center"} px={9}>
-                    <Stack as="nav" spacing={2}>
-                        {navLinks.map((link, index) => (
-                            <NavLink key={index} {...link} onClose={onClose} border={'1px solid black'} />
-                        ))}
-                        <Link to={'/Community'} textDecora>
-                            <p>Community</p>
-                        </Link>
-                        {/* <Stack pl={2} spacing={1} mt={'0 !important'}>
-                            {dropdownLinks.map((link, index) => (
-                                <NavLink key={index} {...link} onClose={onClose} />
+                <Drawer placement={'right'} onClose={onClose} isOpen={isOpen} size={'xs'} >
+                    <DrawerOverlay />
+                    <DrawerContent>
+                        <DrawerCloseButton />
+                        <DrawerBody textAlign='center' display={'flex'} flexDirection={'column'} alignItems={'center'} padding={'7vh 0 '}>
+                            {navLinks.map((link, index) => (
+                                <div>
+                                    <NavLink key={index} {...link} onClose={onClose} border={'1px solid black'} />
+                                    <br />
+                                </div>
                             ))}
-                        </Stack> */}
-                    </Stack>
-                </Box>
+                            <Link to={'/Community'} textDecoration={'none'}>
+                                <p>Community</p>
+                            </Link>
+                            {signin ? <Button onClick={handleSignOut} bg={"#FBBC05"} size="md" borderRadius={"20px"} display={{ base: 'block', md: 'none' }} _hover={"none"}    >
+                                Log out
+                            </Button> :
+                                <Button onClick={() => { Navigate('/signup') }} bg={"#FBBC05"} size="md" borderRadius={"20px"} display={{ base: 'block', md: 'none' }} _hover={"none"}   color={'white'}>
+                                    Sign up
+                                </Button>}
+                        </DrawerBody>
+                    </DrawerContent>
+                </Drawer>
             ) : null}
         </Box>
     );
@@ -128,24 +130,13 @@ const NavLink = ({ name, path, onClose }) => {
             lineHeight="inherit"
             _hover={{
                 textDecoration: 'none',
-                color:"black",
-                // color: useColorModeValue('blue.500', 'blue.200')
-                fontSize:'lg'
+                color: "black",
+                // color: useColorModeValue('blue.500', 'blue.200'),
+                fontSize: 'lg'
             }}
             onClick={() => onClose()}
         >
-            {name}
-        </Link>
-    );
-};
-
-// Dropdown MenuLink Component
-const MenuLink = ({ name, path, onClose }) => {
-    return (
-        <Link href={path} onClick={() => onClose()}>
-            <MenuItem _hover={{ textDecoration:"none",color:"black" }}>
-                <Text>{name}</Text>
-            </MenuItem>
+            <Text size={'4xl'}>{name}</Text>
         </Link>
     );
 };
--- client/src/context/ParentContext.jsx
+++ client/src/context/ParentContext.jsx
@@ -1,14 +1,31 @@
-import React, { createContext, useState } from 'react'
+import { onAuthStateChanged } from 'firebase/auth';
+import React, { createContext, useEffect, useState } from 'react';
+import { auth } from '../firebase/config';
+// import firebase from 'firebase/app';
+// import 'firebase/auth';
 
-export const AppContext = createContext()
+export const AppContext = createContext();
 
 const ParentContext = ({ children }) => {
-  const [userData,setUserData] = useState({})
-  const [signin,setSignin]=useState(false)
+  const [userData, setUserData] = useState({});
+  const [signin, setSignin] = useState(false);
 
-  return <AppContext.Provider value={{signin,setSignin,setUserData,userData }}>
-    {children}
-  </AppContext.Provider>
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUserData(currentUser);
+      setSignin(true)
+      console.log('Userdata', currentUser)
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  return (
+    <AppContext.Provider value={{ signin, setSignin, setUserData, userData }}>
+      {children}
+    </AppContext.Provider>
+  );
 }
 
 export default ParentContext
