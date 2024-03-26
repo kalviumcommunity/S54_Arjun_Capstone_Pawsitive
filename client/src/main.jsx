06693437ed0dbdc122cecbb901c1087f0d59865d
Arjun Kotha <141010334+Phoenix1531@users.noreply.github.com>
2024-03-26 17:47:33 +0530

    message-section implemented

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import ParentContext from './context/ParentContext.jsx'
+import AuthContext from '../src/context/AuthContext.jsx'
 import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
-      <ParentContext>
+      <AuthContext>
         <BrowserRouter>
           <App />
         </BrowserRouter>
-      </ParentContext>
+      </AuthContext>
   </>
 )
