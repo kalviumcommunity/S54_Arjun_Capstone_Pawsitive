d17cd9fa705f1ac6a0e7536ac43726c0a7a8b674
Arjun Kotha <141010334+Phoenix1531@users.noreply.github.com>
2024-04-12 14:54:03 +0530

    blog-like

--- backend/Routes/BlogRoutes.js
+++ backend/Routes/BlogRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express")
 const router = express.Router()
 
-const { createBlog,getAllBlogs,getBlog,postComment, getComments} = require("../Controllers/BlogController")
+const { createBlog,getAllBlogs,getBlog,postComment, getComments,likeBlog} = require("../Controllers/BlogController")
 
 router.post("/", createBlog)
 router.get("/all", getAllBlogs)
 router.get("/:pid", getBlog)
+router.post("/:blogId/like", likeBlog);
 // router.put("/:pid", updateBlog)
 router.post('/:blogId/comments', postComment)
 router.get('/:blogId/comments', getComments)
