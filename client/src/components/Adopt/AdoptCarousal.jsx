e824967e2514fe1fc913c8380ce407d4d18af8c6
Arjun Kotha <141010334+Phoenix1531@users.noreply.github.com>
2024-06-30 19:46:54 +0530

    razorpay

--- client/src/components/Adopt/AdoptCarousal.jsx
+++ client/src/components/Adopt/AdoptCarousal.jsx
@@ -8,7 +8,7 @@ import {
 } from '@chakra-ui/react';
 import pic1 from '../../assets/adoptPics/pic1.svg'
 import pic2 from '../../assets/adoptPics/pic2.svg'
-import adoptimg from '../../assets/adoptImg 1.svg'
+import pic3 from '../../assets/pic3.jpg'
 
 const cards = [
     {
@@ -19,10 +19,10 @@ const cards = [
         text: "Find your new best friend",
         image: pic2,
     },
-    // {
-    //     text: "Find your new best friend",
-    //     image: adoptimg,
-    // },
+    {
+        text: "Find your new best friend",
+        image: pic3,
+    },
 ];
 
 export default function AdoptCarousel() {
@@ -37,8 +37,8 @@ export default function AdoptCarousel() {
     }, []);
 
     return (
-        <Box position="relative" overflow="hidden" minH={{base:"20vh",md:"55vh"}} maxH={{base:"40vh",md:"55vh"}}>
-            <Image src={cards[currentSlide].image} minH={{base:"30vh"}} width="100%" height="100%" objectFit="cover" boxShadow="inset 0 0 0 2000px rgba(0,0,0,0.6)" />
+        <Box position="relative" overflow="hidden" minH={{ base: "20vh", md: "55vh" }} maxH={{ base: "40vh", md: "55vh" }}>
+            <Image src={cards[currentSlide].image} minH={{ base: "30vh" }} width="100%" height="100%" objectFit="cover" boxShadow="inset 0 0 0 2000px rgba(0,0,0,0.6)" />
             <Center position="absolute" top="75%" left="50%" transform="translate(-50%, -50%)" textAlign="center">
                 <Text fontSize={{ base: '2xl', lg: '6xl' }} color="white" textShadow="2px 2px 4px rgba(0, 0, 0, 0.6)">
                     {cards[currentSlide].text}
